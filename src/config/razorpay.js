f7c585f89097ee2215b6eff873175f486f0a8c3b
shreyas.s <shreyas.s@diatoz.com>
2026-06-10 15:40:54 +0530

    correction of razorpay

--- src/config/razorpay.js
+++ src/config/razorpay.js
@@ -1,10 +1,16 @@
-const razorpay = require("razorpay")
-const dotenv = require("dotenv")
-dotenv.config();
+const Razorpay = require("razorpay");
+require("dotenv").config();
 
-const razorpay = new RazorPay({
+if (
+    !process.env.RAZORPAY_KEY_ID ||
+    !process.env.RAZORPAY_KEY_SECRET
+) {
+    throw new Error("Razorpay credentials are missing in .env");
+}
+
+const razorpay = new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET
-})
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
 
 module.exports = razorpay;
