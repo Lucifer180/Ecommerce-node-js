044ac0c3e3e9f9dd60f340281ff9bdc08f83a714
shreyas.s <shreyas.s@diatoz.com>
2026-06-10 15:59:46 +0530

    correction of razorpay

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+const js = require("@eslint/js");
+
+module.exports = [
+    js.configs.recommended,
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "commonjs",
+        },
+        rules: {}
+    }
+]
