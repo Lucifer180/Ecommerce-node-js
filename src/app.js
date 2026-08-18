f44028a48e5efdcda4a0c5481ebe5676ebd52550
shreyas.s <shreyas.s@diatoz.com>
2026-08-18 12:20:01 +0530

    feat: add script to sync MongoDB products into Elasticsearch index

--- src/app.js
+++ src/app.js
@@ -49,7 +49,7 @@ app.use((req, res, next) => {
 app.use(hpp());
 app.use(helmet());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true
 }));
 const cartRoutes = require("./modules/cart/cart.routes");
